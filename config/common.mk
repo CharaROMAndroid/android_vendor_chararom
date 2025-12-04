b6bca89eb71a6f1b51a72370284d0b760781a389
EunoiaOS <235215905+eunoia-committer@users.noreply.github.com>
2025-12-04 22:07:28 +0700

    config: Don't build bootanimation for now
    Change-Id: I31a02eca3e6fe673913250c54e55dce092c950b1

--- config/common.mk
+++ config/common.mk
@@ -137,9 +137,9 @@ endif
 # Bootanimation
 TARGET_SCREEN_WIDTH ?= 1080
 TARGET_SCREEN_HEIGHT ?= 1920
-PRODUCT_PACKAGES += \
-    bootanimation.zip \
-    bootanimation-dark.zip
+#PRODUCT_PACKAGES += \
+#    bootanimation.zip \
+#    bootanimation-dark.zip
 
 # Charger
 PRODUCT_PACKAGES += \
