27c6f4c6bd463345b27e33347199a36f327b44d6
Garry Strait <92406354+GarryStraitYT@users.noreply.github.com>
2026-03-01 09:11:58 -0500

    Update versioning for CharaROM release

--- config/version.mk
+++ config/version.mk
@@ -1,17 +1,17 @@
 PRODUCT_VERSION_MAJOR = 16
 PRODUCT_VERSION_MINOR = 0
 
-# Increase CR Version with each major release.
-CR_VERSION := 12.12
+# Increase CharaROM Version with each major release.
+CHARAROM_VERSION := 1.0
 
 # Internal version
-LINEAGE_VERSION := crDroidAndroid-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)-v$(CR_VERSION)
+LINEAGE_VERSION := CharaROM-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)-v$(CHARAROM_VERSION)
 
 # Display version
-LINEAGE_DISPLAY_VERSION := v$(CR_VERSION)-$(shell date +%Y%m%d)
+LINEAGE_DISPLAY_VERSION := v$(CHARAROM_VERSION)-$(shell date +%Y%m%d)
 
 # LineageOS version properties
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.crdroid.build.version=$(CR_VERSION) \
-    ro.crdroid.display.version=$(LINEAGE_DISPLAY_VERSION) \
-    ro.crdroid.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)
+    ro.chara.build.version=$(CHARAROM_VERSION) \
+    ro.chara.display.version=$(LINEAGE_DISPLAY_VERSION) \
+    ro.chara.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)
