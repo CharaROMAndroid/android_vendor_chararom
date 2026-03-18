5f0b4eb467734bfa36ce1096858c503d210195b5
Garry Strait (Chara) <92406354+GarryStraitYT@users.noreply.github.com>
2026-03-18 19:49:38 -0400

    Update common.mk
    Copy of https://github.com/LMODroid/platform_vendor_lmodroid/blob/62195228f940c107235168ff3bae2b759afe6448/config/common.mk#L162

--- config/common.mk
+++ config/common.mk
@@ -160,6 +160,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
 
+# Panic
+PRODUCT_PACKAGES += \
+    Ripple
+
 # Config
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig \
