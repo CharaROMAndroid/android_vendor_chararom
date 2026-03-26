429a0acbcea99f9cae73aedd51e8e9339072baf9
Chara <bionichuman07@gmail.com>
2026-03-25 20:14:49 -0400

    temporarily remove Ripple until we can get it building properly

--- config/common.mk
+++ config/common.mk
@@ -161,7 +161,7 @@ PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
 
 # Panic
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     Ripple
 
 # Config
