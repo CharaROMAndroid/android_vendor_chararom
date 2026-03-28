df63dca74bac5b550e647d317429c061fb3839da
Chara <bionichuman07@gmail.com>
2026-03-28 06:41:41 -0400

    Alpha-specific Changes
    Set version to Hershey, temporarily map ro.crdroid.build.version to ro.chara.build.version, at least until we get everything ready. After which point, the crdroid.build.version prop will be dropped.

--- config/version.mk
+++ config/version.mk
@@ -3,7 +3,7 @@ PRODUCT_VERSION_MINOR = 0
 ANDROID_VERSION = 16
 
 # Increase CharaROM Version with each major release.
-CHARAROM_VERSION := 1.0
+CHARAROM_VERSION := Hershey
 
 # Internal version
 LINEAGE_VERSION := CharaROM-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-Android$(ANDROID_VERSION)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)-v$(CHARAROM_VERSION)
@@ -15,4 +15,5 @@ LINEAGE_DISPLAY_VERSION := v$(CHARAROM_VERSION)-$(shell date +%Y%m%d)
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.chara.build.version=$(CHARAROM_VERSION) \
     ro.chara.display.version=$(LINEAGE_DISPLAY_VERSION) \
-    ro.chara.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)
+    ro.chara.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)\
+    ro.crdroid.build.version=$(CHARAROM_VERSION)\
