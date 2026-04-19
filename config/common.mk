8193921586845e900c5810bfdb0472391651d43c
Chara <bionichuman07@gmail.com>
2026-04-19 02:28:57 -0400

    Add AOSmium WebView to supported webviews list, rename to CharaROM

--- config/common.mk
+++ config/common.mk
@@ -1,6 +1,6 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
-$(call inherit-product-if-exists, vendor/lineage/config/crdroid.mk)
+$(call inherit-product-if-exists, vendor/lineage/config/chararom.mk)
 $(call inherit-product-if-exists, vendor/addons/config.mk)
 
 # Exclude repos from bp scanning
