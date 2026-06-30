71ccfe77576c95f8168bfc66717c335734363235
Chara <bionichuman07@gmail.com>
2026-06-30 12:39:32 -0400

    Deprecate FaceUnlock; General CharaROM Fixes

--- config/chararom.mk
+++ config/chararom.mk
@@ -57,18 +57,6 @@ PRODUCT_PACKAGES += \
     OmniStyle\
     #F-DroidPrivilegedExtension\
 
-
-ifneq ($(TARGET_FACE_UNLOCK_SUPPORTED),false)
-PRODUCT_PACKAGES += \
-    FaceUnlock
-
-PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    ro.face.sense_service=true
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/android.hardware.biometrics.face.xml
-endif
-
 # DeviceAsWebcam
 ifeq ($(TARGET_BUILD_DEVICE_AS_WEBCAM), true)
 PRODUCT_PACKAGES += \
--- config/common.mk
+++ config/common.mk
@@ -178,9 +178,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
 
-# Panic
-#PRODUCT_PACKAGES += \
-    Ripple
 
 # Config
 PRODUCT_PACKAGES += \
