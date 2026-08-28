0a382679b5d7e418122d1041c5c0af180b63dc3f
jpb <jpb44173@gmail.com>
2026-08-28 23:09:58 +0000

    vendor: Fix pixel_2016_exclusive.xml path
    The path points to 'vendor/circle', whereas CharaROM's vendor module path is 'vendor/lineage'.
    This causes a build failure as the xml cannot be found under the 'vendor/circle' directory, wwwwwwwhich doesn't actually exist.

--- config/common.mk
+++ config/common.mk
@@ -105,7 +105,7 @@ PRODUCT_PACKAGES += \
 
 # Unlimited google photos backup
 PRODUCT_COPY_FILES += \
-    vendor/circle/prebuilt/google/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
+    vendor/lineage/prebuilt/google/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
 
 # Enable wireless Xbox 360 controller support
 PRODUCT_COPY_FILES += \
